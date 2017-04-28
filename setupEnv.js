f38c3781e79d47b9f15eb6e970fc07bffe8b2bc4
Enyosolutions <fb@enyosolutions.com>
2017-04-28 10:39:19 +0100

    Update setupEnv.js

--- setupEnv.js
+++ setupEnv.js
@@ -1,4 +1,4 @@
-module.exports = function (context) {
+module.exports = function(context) {
     var util = require('util'),
         fs = require('fs'),
         path = require('path');
@@ -7,6 +7,7 @@ module.exports = function (context) {
     var platforms = context.opts.platforms,
         cliCommand = context.cmdLine,
         fileName = 'src/config/envs/config.',
+        configXmlPath = path.normalize(path.join(context.opts.projectRoot, "config.xml")),
         filePath = path.normalize(path.join(context.opts.projectRoot, fileName));
     // hook configuration
     var idx = cliCommand.indexOf('--app-env=');
@@ -24,15 +25,50 @@ module.exports = function (context) {
 
     fs.readFile(filePath + env + '.json', {
         encoding: 'utf8'
-    }, function (err, data) {
+    }, function(err, data) {
         if (err) throw err;
         setEnvInfo(data);
     });
 
+    fs.readFile(configXmlPath, {
+        encoding: 'utf8'
+    }, function(err, data) {
+        if (err) throw err;
+        updateBundleId(data, env);
+    });
+
+    /**
+    @descripttion  write
+    */
     function setEnvInfo(info) {
-        fs.writeFile(filePath + 'json', info, function (err, data) {
+        fs.writeFile(filePath + 'json', info, function(err, data) {
             if (err) throw err;
             console.log("settings updated with env", env);
+        });
+    }
+
+    /**
+    @descripttion  change bundle id according to env.
+    */
+    function updateBundleId(info, env) {
+        var bundleId;
+        switch (env) {
+            default:
+                case "dev":
+                bundleId = "fr.dvmobile.entreprise.sdj-sav-dev";
+            break;
+            case "qa":
+                    bundleId = "fr.dvmobile.entreprise.sdj-sav-qa";
+                break;
+            case "prod":
+                    bundleId = "fr.dvmobile.entreprise.sdj-sav-prod";
+                break;
+        }
+        info = info.replace(/fr\.dvmobile\.entreprise\.sdj\-sav\-(prod|qa|dev)/, bundleId);
+        console.log(info);
+        fs.writeFile(configXmlPath, info, function(err, data) {
+            if (err) throw err;
+            console.log("config.xml updated with env", env, bundleId);
         });
     }
 
